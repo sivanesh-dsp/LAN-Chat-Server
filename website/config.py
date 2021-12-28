e5429a11082c635ae5b07f0ed2103979f7fb9f2a
Sivanesh B <60127773+sivanesh-dsp@users.noreply.github.com>
2021-12-28 12:22:43 +0530

    Add files via upload

---
+++ website/config.py
@@ -0,0 +1,18 @@
+import socket
+from dotenv import load_dotenv
+from pathlib import Path  # python3 only
+import os
+
+# set path to env file
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+class Config:
+    """Set Flask configuration vars from .env file."""
+
+    # Load in enviornemnt variables
+    TESTING = os.getenv('TESTING')
+    FLASK_DEBUG = os.getenv('FLASK_DEBUG')
+    SECRET_KEY = ('SECRET_KEY')
+    SERVER = socket.gethostbyname(socket.gethostname())
